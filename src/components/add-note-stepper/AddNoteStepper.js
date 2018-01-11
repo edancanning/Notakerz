2e294b17c1698843f5edc5eb1a7f9874ae5357e0
Edan Canning <edancanning@ufl.edu>
2018-01-11 16:33:10 -0500

    working on form validation for ad note form

--- src/components/add-note-stepper/AddNoteStepper.js
+++ src/components/add-note-stepper/AddNoteStepper.js
@@ -11,12 +11,6 @@ import {
 
 import "./addNoteStepper.css";
 
-var steps = [
-  "Note information",
-  "For each ad campaign ",
-  "For each ad campaign "
-];
-
 var AddNoteStepper = props => (
   <div className="add-note-stepper-component">
     <Stepper
@@ -24,34 +18,16 @@ var AddNoteStepper = props => (
       orientation="vertical"
       className="stepper"
     >
-      {steps.map((label, index) => {
+      {props.steps.map((label, index) => {
         return (
           <Step className="step" key={label}>
             <StepLabel>{label}</StepLabel>
-            <StepContent>
-              {props.getStep(index)}
-              <div className="step-buttons">
-                <Button
-                  disabled={props.activeStep === 0}
-                  onClick={props.handleBack}
-                >
-                  Back
-                </Button>
-                <Button
-                  className="next-button"
-                  raised
-                  color="primary"
-                  onClick={props.handleNext}
-                >
-                  {props.activeStep === steps.length - 1 ? "Finish" : "Next"}
-                </Button>
-              </div>
-            </StepContent>
+            <StepContent>{props.getStep(index)}</StepContent>
           </Step>
         );
       })}
     </Stepper>
-    {props.activeStep === steps.length && (
+    {props.activeStep === props.steps.length && (
       <Paper square elevation={0}>
         <Typography>All steps completed - you&quot;re finished</Typography>
         <Button onClick={props.handleReset}>Reset</Button>
--- src/containers/add-note-modal/AddNoteModal.js
+++ src/containers/add-note-modal/AddNoteModal.js
@@ -14,6 +14,7 @@ import {
   Toolbar,
   IconButton,
   TextField,
+  FormHelperText,
   InputAdornment
 } from "material-ui";
 import axios from "axios";
@@ -26,12 +27,22 @@ class AddNoteModal extends React.Component {
   constructor(props) {
     super();
     this.state = {
+      steps: [
+        "Note information",
+        "For each ad campaign ",
+        "For each ad campaign "
+      ],
       activeStep: 0,
-      university: "University of Florida",
+      university: "",
       course: "",
       title: "",
       description: "",
       price: "",
+      universityError: false,
+      courseError: false,
+      titleError: false,
+      descriptionError: false,
+      priceError: false,
       courses: [],
       universities: []
     };
@@ -60,7 +71,62 @@ class AddNoteModal extends React.Component {
   };
 
   handleInputChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({
+      [event.target.name]: event.target.value,
+      [event.target.name + "Error"]: false
+    });
+  };
+
+  handlePriceChange = event => {
+    var value = event.target.value;
+    if (!isNaN(value) && value.length <= 4) {
+      if (value.length === 2) {
+        if (value[1] === ".") {
+          value = value[0];
+        } else {
+          value = value[0] + "." + value[1];
+        }
+      }
+      this.setState({
+        price: value,
+        priceError: false
+      });
+    }
+  };
+
+  handleFormNext = () => {
+    var verified = true;
+
+    if (this.state.university === "") {
+      verified = false;
+      this.setState({ universityError: true });
+    }
+
+    if (this.state.course === "") {
+      verified = false;
+      this.setState({ courseError: true });
+    }
+
+    if (this.state.title === "") {
+      verified = false;
+      this.setState({ titleError: true });
+    }
+
+    if (this.state.description === "") {
+      verified = false;
+      this.setState({ descriptionError: true });
+    }
+
+    if (this.state.price === "") {
+      verified = false;
+      this.setState({ priceError: true });
+    }
+
+    if (verified) {
+      this.setState({
+        activeStep: this.state.activeStep + 1
+      });
+    }
   };
 
   //
@@ -86,87 +152,131 @@ class AddNoteModal extends React.Component {
   getStep = index => {
     if (index === 0) {
       return (
-        <form className="form" autoComplete="off">
-          <FormControl className="input-field">
-            <InputLabel htmlFor="university">University</InputLabel>
-            <Select
-              value={this.state.university}
-              onChange={this.handleInputChange}
-              input={<Input name="university" id="university" />}
+        <div>
+          <form className="form" autoComplete="off">
+            <FormControl
+              required
+              className="input-field"
+              error={this.state.universityError}
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {this.state.universities.map(university => (
-                <MenuItem key={university._id} value={university._id}>
-                  {university.name}
+              <InputLabel htmlFor="university">University</InputLabel>
+              <Select
+                value={this.state.university}
+                onChange={this.handleInputChange}
+                input={<Input name="university" id="university" />}
+              >
+                <MenuItem value="">
+                  <em>None</em>
                 </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl className="input-field">
-            <InputLabel htmlFor="course">Course</InputLabel>
-            <Select
-              value={this.state.course}
-              onChange={this.handleInputChange}
-              input={<Input name="course" id="course" />}
+                {this.state.universities.map(university => (
+                  <MenuItem key={university._id} value={university._id}>
+                    {university.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl
+              required
+              className="input-field"
+              error={this.state.courseError}
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              {this.state.courses.map((course, index) => (
-                <MenuItem key={course._id} value={course._id}>
-                  {courseToTitle(
-                    course.code,
-                    course.semester,
-                    course.year,
-                    course.professor
-                  )}
+              <InputLabel htmlFor="course">Course</InputLabel>
+              <Select
+                value={this.state.course}
+                onChange={this.handleInputChange}
+                input={<Input name="course" id="course" />}
+              >
+                <MenuItem value="">
+                  <em>None</em>
                 </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            name="title"
-            className="input-field"
-            required
-            id="course-title"
-            label="Course Title"
-            value={this.state.title}
-            margin="normal"
-            helperText="Min 10 characters"
-            maxLength={20}
-            onChange={this.handleInputChange}
-          />
-          <TextField
-            name="description"
-            className="input-field"
-            required
-            id="course-description"
-            label="Course Description"
-            defaultValue=""
-            margin="normal"
-            multiline
-            maxLength={200}
-            onChange={this.handleInputChange}
-            helperText="Min 30 characters"
-          />
-          <FormControl className="input-field">
-            <InputLabel htmlFor="price">Price</InputLabel>
-            <Input
-              id="price"
-              value={this.state.price}
+                {this.state.courses.map((course, index) => (
+                  <MenuItem key={course._id} value={course._id}>
+                    {courseToTitle(
+                      course.code,
+                      course.semester,
+                      course.year,
+                      course.professor
+                    )}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              error={this.state.titleError}
+              name="title"
+              className="input-field"
+              required
+              id="course-title"
+              label="Course Title"
+              value={this.state.title}
+              margin="normal"
+              helperText="Min 10 characters"
+              maxLength="2"
               onChange={this.handleInputChange}
-              name="price"
-              startAdornment={
-                <InputAdornment position="start">$</InputAdornment>
-              }
             />
-          </FormControl>
-        </form>
+            <TextField
+              error={this.state.descriptionError}
+              name="description"
+              className="input-field"
+              required
+              id="course-description"
+              label="Course Description"
+              defaultValue=""
+              margin="normal"
+              multiline
+              maxLength="100"
+              onChange={this.handleInputChange}
+              helperText="Min 30 characters"
+            />
+            <FormControl
+              required
+              className="input-field"
+              error={this.state.priceError}
+            >
+              <InputLabel htmlFor="price">Price</InputLabel>
+              <Input
+                id="price"
+                value={this.state.price}
+                onChange={this.handlePriceChange}
+                name="price"
+                startAdornment={
+                  <InputAdornment position="start">$</InputAdornment>
+                }
+              />
+              <FormHelperText>Max $9.99</FormHelperText>
+            </FormControl>
+          </form>
+          <div className="step-buttons">
+            <Button disabled onClick={this.handleBack}>
+              Back
+            </Button>
+            <Button
+              className="next-button"
+              raised
+              color="primary"
+              onClick={this.handleFormNext}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
       );
     } else if (index === 1) {
-      return <div>yo2</div>;
+      return (
+        <div>
+          <div className="step-buttons">
+            <Button onClick={this.handleBack}>Back</Button>
+            <Button
+              className="next-button"
+              raised
+              color="primary"
+              onClick={this.handleNext}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
+      );
     } else if (index === 2) {
       return <div>yo3</div>;
     }
@@ -200,6 +310,7 @@ class AddNoteModal extends React.Component {
           </DialogTitle>
           <DialogContent className="dialog-content">
             <AddNoteStepper
+              steps={this.state.steps}
               getStep={this.getStep}
               handleReset={this.handleReset}
               handleBack={this.handleBack}
